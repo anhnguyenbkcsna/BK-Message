b1698f6e0f71bf1a27f0b71d47919d647cb69923
nhaTahn <74145826+nhaTahn@users.noreply.github.com>
2022-11-13 15:03:12 +0700

    Valid nho nho :(

--- client/src/components/Chat/Home.js
+++ client/src/components/Chat/Home.js
@@ -7,44 +7,56 @@ import { socket } from '../../services/socket';
 const Home = () => {
     const navigate = useNavigate()
     const [userName, setUserName] = useState("")
-
+    const [passWord, setPassWord] = useState("")
     const handleSubmit = (e) => {
         e.preventDefault()
-        localStorage.setItem("userName", userName)
-        socket.emit("newUser", { userName, socketID: socket.id })
-        navigate("/chat")
+        if (userName.length > 0) {
+            localStorage.setItem("userName", userName)
+            socket.emit("newUser", { userName, socketID: socket.id })
+            navigate("/chat")
+        }
     }
     return (
-        // <form className='home__container' onSubmit={handleSubmit}>
-        // <h2 className='home__header'>Your name in the chat</h2>
-        <Box 
-            component='form' 
-            noValidate
-            sx={{ '& > :not(style)': { m: 1, width: '25ch' }, }}
+        <Box
+            component='form' noValidate container
+            backgroundColor="#AEBDCA"
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            minHeight="50vh"
+            mt="20vh"
+            sx={{
+                '& > :not(style)': {
+                    m: 1, width: '25ch',
+                },
+                flexDirection: 'column'
+            }}
             onSubmit={handleSubmit}
         >
-            <TextField 
+            <h1 > Sign In Chat App</h1>
+            <TextField
                 type="text"
-                id="username" 
-                name="username" 
+                id="username"
+                name="username"
                 value={userName}
-                label="Username" 
+                label="Username"
                 variant="outlined"
-                onChange={e => setUserName(e.target.value)} 
+                error={userName.length === 0}
+                helperText={!userName.length ? 'required' : ''}
+                onChange={e => setUserName(e.target.value)}
+            />
+            <TextField
+                type="text"
+                id="password"
+                name="password"
+                value={passWord}
+                label="Password"
+                variant="outlined"
+                error={passWord.length === 0}
+                onChange={e => setPassWord(e.target.value)}
             />
             <Button variant="contained" color="success" onClick={handleSubmit}>Enter</Button>
-        </Box>
-        /* <input type="text"
-            minLengt="1"
-            name="username"
-            id='username'
-            className='username__input'
-            value={userName}
-            onChange={e => setUserName(e.target.value)}
-        /> */
-
-        /* <button className='home__cta'>SIGN IN</button> */
-        // </form>
+        </Box >
     )
 }
 
