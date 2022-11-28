e8c5a5f40c438cc5ffcf1350c383e6b4c0876bd9
SUN-0207 <cuthanhson27@gmail.com>
2022-11-29 00:42:05 +0700

    change index.js

--- client/src/components/SignIn/index.js
+++ client/src/components/SignIn/index.js
@@ -1,82 +1,89 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import { makeStyles } from "@mui/styles";
-import { useNavigate } from "react-router-dom"
-import { Button, TextField, FormControl, Link } from '@mui/material'
-import { socket } from '../../services/socket';
+import { useNavigate } from "react-router-dom";
+import {
+  Button,
+  TextField,
+  FormControl,
+  Link,
+  Card,
+  title,
+  Box,
+  Grid,
+} from "@mui/material";
+import { socket } from "../../services/socket";
+import { Container } from "@mui/system";
 
-const useStyles = makeStyles(() => ({
-  container: {
-    width: '100%',
-    height: '100%',
-  },
-  form: {
-    margin: "0 auto",
-    backgroundColor:"#AEBDCA",
-    alignItems: 'center',
-    display: "flex",
-    justifyContent:"center",
-    minHeight:"50vh",
-    width: "50vw",
-    flexDirection: 'column',
-    gap: "20px",
-  },
-  accountProblems: {
-    width: "35%",
-    display: "flex",
-    gap: "20%",
-  },
-}))
+const useStyles = makeStyles(() => ({}));
 
 const SignIn = () => {
   const styles = useStyles();
-  const navigate = useNavigate()
-  const [userName, setUserName] = useState("")
-  const [passWord, setPassWord] = useState("")
-const handleSubmit = (e) => {
-    e.preventDefault()
+  const navigate = useNavigate();
+  const [userName, setUserName] = useState("");
+  const [passWord, setPassWord] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
     if (userName.length > 0) {
-        localStorage.setItem("userName", userName)
-        socket.emit("newUser", { userName, socketID: socket.id })
-        navigate("/chat")
+      localStorage.setItem("userName", userName);
+      socket.emit("newUser", { userName, socketID: socket.id });
+      navigate("/chat");
     }
-}
-return (
-  <div className={styles.container}>
-    <FormControl
-      className = {styles.form}
-        onSubmit={handleSubmit}
-    >
-    <h1 > Sign In Chat App chua co cai password</h1>
-    <TextField
-        type="text"
-        id="username"
-        name="username"
-        value={userName}
-        label="Username"
-        variant="outlined"
-        onChange={e => setUserName(e.target.value)}
-    />
-      <TextField
-          type="password"
-          id="password"
-          name="password"
-          value={passWord}
-          label="Password"
-          variant="outlined"
-          onChange={e => setPassWord(e.target.value)}
-      />
-      <div className={styles.accountProblems}>
-      <Link href="signup" underline="hover">
-        Create account
-      </Link>
-      <Link href="forgetpassword" underline="hover">
-        Forget Password
-      </Link>
-      </div>
-      <Button variant="contained" color="success" onClick={handleSubmit}>Enter</Button>
-    </FormControl>
-  </div>
-  )
-}
+  };
+  return (
+    <div>
+      <Grid container spacing={2}>
+        <Grid item xs={6} md={8}>
+          <Item>xs=6 md=8</Item>
+        </Grid>
+        <Grid item xs={6} md={4}>
+          <Item>xs=6 md=4</Item>
+        </Grid>
+        <Grid item xs={6} md={4}>
+          <Item>xs=6 md=4</Item>
+        </Grid>
+        <Grid item xs={6} md={8}>
+          <Item>xs=6 md=8</Item>
+        </Grid>
+      </Grid>
+    </div>
+    // <div className={styles.container}>
+    //   <Card>
 
-export default SignIn
+    //   </Card>
+    //   <FormControl onSubmit={handleSubmit}>
+    //     <h1>LOGIN</h1>
+    //     <TextField
+    //       type="text"
+    //       id="username"
+    //       name="username"
+    //       value={userName}
+    //       label="Username"
+    //       variant="outlined"
+    //       onChange={(e) => setUserName(e.target.value)}
+    //     />
+    //     <TextField
+    //       type="password"
+    //       id="password"
+    //       name="password"
+    //       value={passWord}
+    //       label="Password"
+    //       variant="outlined"
+    //       onChange={(e) => setPassWord(e.target.value)}
+    //     />
+    //     <div>
+    //       <Link href="signup" underline="hover">
+    //         Create account
+    //       </Link>
+    //       <Link href="forgetpassword" underline="hover">
+    //         Forget Password
+    //       </Link>
+    //     </div>
+    //     <Button variant="contained" color="success" onClick={handleSubmit}>
+    //       Enter
+    //     </Button>
+    //   </FormControl>
+    // </div>
+  );
+};
+
+export default SignIn;
